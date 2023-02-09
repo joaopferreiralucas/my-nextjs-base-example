d1d6e61fa1bf1de1692b6aa8f3083acd5d3b78fc
Joao P Ferreira Lucas <joaopferreiralucas@gmail.com>
2023-02-08 21:11:24 -0300

    base finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from '../styles/theme';
+import { GlobalStyles } from '../styles/global-styles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
